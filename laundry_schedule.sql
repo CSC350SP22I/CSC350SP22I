1ad0c6b959da39f00fdc882d950ac0cca3245699
CSC350SP22I <103593230+CSC350SP22I@users.noreply.github.com>
2022-05-21 12:01:55 -0400

    Add files via upload

--- laundry_schedule.sql
+++ laundry_schedule.sql
@@ -27,6 +27,7 @@ CREATE TABLE `schedule` (
   `Slot` varchar(15) DEFAULT NULL,
   `Date` varchar(8) DEFAULT NULL,
   PRIMARY KEY (`Apartment`),
+  UNIQUE KEY `Apartment_UNIQUE` (`Apartment`),
   CONSTRAINT `fk_Schedule_Credentials` FOREIGN KEY (`Apartment`) REFERENCES `credentials` (`Apartment`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -40,4 +41,4 @@ CREATE TABLE `schedule` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-05-21 11:48:25
+-- Dump completed on 2022-05-21 12:00:50
